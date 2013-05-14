4829cdbdfbb7a85fdfd678159fb9d077becc9746
8one6 <jbrodie@8one6.com>
2013-05-14 13:44:35 -0400

    Using shortcuts for template rendering

--- jrbsite/views.py
+++ jrbsite/views.py
@@ -1,6 +1,4 @@
-from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse
+from django.shortcuts import render
 import datetime
 
 def hello(request):
@@ -8,9 +6,7 @@ def hello(request):
 	
 def current_datetime(request):
 	now = datetime.datetime.now()
-	t = get_template('current_datetime.html')
-	html = t.render(Context({'current_date' : now}))
-	return HttpResponse(html)
+	return render(request, 'current_datetime.html', { 'current_date' : now })
 	
 def hours_ahead(request, offset):
 	try: 
